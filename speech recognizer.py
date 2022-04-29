f6552338313e929ce85fe20740866fe39b5018dc
Kyo/coolboy <85298936+Apoorv-create@users.noreply.github.com>
2022-04-29 20:27:43 +0530

    Add files via upload

--- speech recognizer.py
+++ speech recognizer.py
@@ -2,9 +2,14 @@ from tkinter import *
 import speech_recognition as sr
 import pyttsx3
 from datetime import datetime
+import subprocess
 
 root = Tk()
 root.geometry("500x500")
+root.configure(background="Light Blue")
+
+label = Label(root, text = "Welcome to your Personal Desktop Assistant", bg="Light Blue", font=("Bahnschrift Light", 15, "bold"))
+label.place(relx = 0.5, rely = 0.1, anchor=CENTER)
 
 text_to_speech =pyttsx3.init()
 
@@ -38,6 +43,20 @@ def respond(voice_data):
         current_time = now.strftime("%H:%M:%S")
         speak(current_time)
         print(current_time)
-        
-r_audio()
+    if "search" in voice_data: 
+        speak("Opening Google")
+        print("Opening Google")
+        webbrowser.get().open("https://google.com")
+    if "videos" in voice_data:
+        speak("Opening Youtube")
+        print("Opening Youtube")
+        webbrowser.get().open("https://youtube.com")
+    if "text editor" in voice_data:
+        speak("Opening Notepad")
+        print("Opening Notepad")
+        subprocess.Popen(["notepad.exe"])
+
+btn = Button(root, text = "start", bg = "red3", fg = "white", padx = 10, pady = 1, font=("Arial",11, "bold"), relief = FLAT, command=r_audio())
+btn.place(relx = 0.5, rely = 0.5, anchor=CENTER)
+
 root.mainloop()
